712eca2f47d1bb19ff6c4311a47a9989e3fa5ab7
Ivan Krasin <imkrasin@gmail.com>
2022-11-17 23:20:14 -0800

    Initialize reset_canary to zero. This will likely not have any effect in the silicon, but it helps the test to pass. The first test case (do nothing, outputs are zeros) pass

--- src/test.py
+++ src/test.py
@@ -11,9 +11,10 @@ async def test_zeros_by_default(dut):
 
   dut._log.info("first cycle")
   dut.pset.value = 0
+  await ClockCycles(dut.clk, 1)
 
   dut._log.info("check that all pins are zeros and stay like that")
   for i in range(10):
     await ClockCycles(dut.clk, 1)
-    print("dut.out_pwm: {}".format(dut.pwm_out.value))
+    # print("dut.out_pwm: {}".format(dut.pwm_out.value))
     assert int(dut.pwm_out.value) == 0
