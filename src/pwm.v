422827b9a1d891a9d09cdaa466abd49db245e7b3
Ivan Krasin <imkrasin@gmail.com>
2022-11-24 00:07:20 -0800

    Add tests for set/get PWM level, they pass

--- src/pwm.v
+++ src/pwm.v
@@ -23,7 +23,8 @@ module krasin_tt02_verilog_spi_7_channel_pwm_driver (
   reg [2:0] spi_counter;
   // is_writing is set if we received a write command.
   reg is_writing;
-  reg [1:0] write_addr;
+  reg is_reading;
+  reg [2:0] cur_addr;
 
   // Buffer from mosi.
   reg [7:0] in_buf;
@@ -31,7 +32,7 @@ module krasin_tt02_verilog_spi_7_channel_pwm_driver (
   reg [7:0] out_buf;
 
   // out_buf is advanced on each falling sclk.
-  assign miso = out_buf[0];
+  assign miso = out_buf[7];
 
   // 8-bit PWM counter that goes from 0 to 254.
   reg [7:0] counter;
@@ -58,6 +59,7 @@ module krasin_tt02_verilog_spi_7_channel_pwm_driver (
   assign pwm_out[6] = is_on(pwm_level[6], counter);
 
   // external clock is 1000Hz.
+  // PWM logic.
   always @(posedge clk) begin
     // if reset, set counter and pwm levels to 0
     if (reset) begin
@@ -69,12 +71,6 @@ module krasin_tt02_verilog_spi_7_channel_pwm_driver (
       pwm_level[4] <= 0;
       pwm_level[5] <= 0;
       pwm_level[6] <= 0;
-      in_buf <= 0;
-      out_buf <= 0;
-      prev_sclk <= 0;
-      spi_counter <= 0;
-      is_writing <= 0;
-      write_addr <= 0;
     end else begin // if (reset)
       if (counter == 254) begin
         // Roll over.
@@ -83,58 +79,65 @@ module krasin_tt02_verilog_spi_7_channel_pwm_driver (
         // increment counter
         counter <= counter + 1'b1;
       end
-      if (cs) begin
-        // The chip is not selected. Reset all SPI registers.
-        in_buf <= 0;
-        out_buf <= 0;
-        prev_sclk <= 0;
-        spi_counter <= 0;
-        is_writing <= 0;
-        write_addr <= 0;
-      end else begin // if(cs)
-        // The chip is selected.
-        if (prev_sclk != sclk) begin
-          // SPI clock changed. On rising edge we read from mosi, on falling edge, we write to miso.
-          if (sclk) begin
-            // Rising SCLK edge: reading from mosi.
-            in_buf <= (in_buf << 1) | mosi;
-            spi_counter <= spi_counter + 1'b1;
-          end else begin // if (sclk)
-            // Falling SCLK edge
-            if (spi_counter == 0) begin
-              if (is_writing) begin
-                // Writing. We saved the write_addr after reading the first byte.
-                if (write_addr <= 6) begin
-                  pwm_level[write_addr] <= in_buf;
-		end // if (write_addr <= 6)
-                // We output the saved value and reset.
-                out_buf <= in_buf;
-                is_writing <= 0;
-                write_addr <= 0;
-              end else begin // if (is_writing)
-                if (in_buf[7]) begin
-                  // We're writing, but the value will come as the next byte.
-                  is_writing <= 1;
-                  write_addr <= in_buf[2:0];
-                end else begin // if (in_buf[7])
-                  // Reading.
-                  // We need to output a new byte.
-                  // Address is in the two lowest bits of in_buf.
-		  if (in_buf[2:0] <= 6) begin
-                    out_buf <= pwm_level[in_buf[2:0]];
-		  end else begin // if (in_buf[2:0] <= 6)
-                    out_buf <= 0;
-		  end
-                end
-              end // if (is_writing)
-            end else begin // if (spi_counter == 0)
-              // Advancing out_buf, so that miso sees a new value.
-             out_buf <= out_buf >> 1;
-            end
-          end
-          prev_sclk <= sclk;
-        end
-      end // else: !if(cs)
-    end
+    end // if (reset)
+  end // always @ (poseedge clk)
+
+  // SPI reset logic.
+  always @(posedge clk) begin
+    if (reset || cs) begin
+      // The chip is not selected or we are being reset. Reset all SPI registers.
+      in_buf <= 0;
+      out_buf <= 0;
+      prev_sclk <= 0;
+      spi_counter <= 0;
+      is_writing <= 0;
+      is_reading <= 0;
+      cur_addr <= 0;
+    end // if (reset || cs)
+  end // always @ (posedge clk)
+
+  // regular SPI logic.
+  always @(posedge clk) begin
+    if (~reset && ~cs && (prev_sclk != sclk)) begin
+      // The chip is selected and the SPI clock changed.
+      // On rising edge we read from mosi, on falling edge, we write to miso.
+      if (sclk) begin
+        // Rising SCLK edge: reading from mosi.
+        in_buf <= (in_buf << 1) | mosi;
+        spi_counter <= spi_counter + 1'b1;
+      end else begin // if (sclk)
+        // Falling SCLK edge
+        if ((spi_counter == 0) && is_writing) begin
+          // Writing. We saved the cur_addr after reading the first byte.
+	  if (cur_addr <= 6) begin
+            pwm_level[cur_addr] <= in_buf;
+	  end
+          is_writing <= 0;
+          is_reading <= 1;
+        end // if ((spi_counter == 0) && is_writing
+	if ((spi_counter == 0) && ~is_writing) begin
+          if (in_buf[7]) begin
+            // We're writing, but the value will come as the next byte.
+            is_writing <= 1;
+	  end else begin
+            is_reading <= 1;
+	  end
+          cur_addr <= in_buf[2:0];
+	end // ((spi_counter == 0) && ~is_writing)
+	if ((spi_counter == 1) && is_reading) begin
+          if (cur_addr <= 6) begin
+            out_buf <= pwm_level[cur_addr];
+	  end else begin
+            out_buf <= 0;
+	  end
+          is_reading <= 0;
+          cur_addr <= 0;
+        end else begin // if ((spi_counter == 1) && is_reading)
+          // Advancing out_buf, so that miso sees a new value.
+          out_buf <= out_buf << 1;
+	end
+      end
+      prev_sclk <= sclk;
+    end // if (~reset && ~cs && (prev_sclk != sclk))
   end // always @ (posedge clk)
 endmodule
--- src/test.py
+++ src/test.py
@@ -43,98 +43,157 @@ async def test_zeros_by_default_sclk(dut):
       dut.sclk.value = 1
     await ClockCycles(dut.clk, 1)
 
-    print("i={}, dut.out_pwm: {}, miso: {}".format(i, dut.pwm_out.value, dut.miso.value))
+    #print("i={}, dut.out_pwm: {}, miso: {}".format(i, dut.pwm_out.value, dut.miso.value))
     assert int(dut.pwm_out.value) == 0
     assert int(dut.miso.value) == 0
 
-#@cocotb.test()
-#async def test_pwm0_full_on(dut):
-#  dut._log.info("start")
-#  clock = Clock(dut.clk, 10, units="us")
-#  cocotb.start_soon(clock.start())
-#
-#  dut._log.info("set pwm0=7")
-#  dut.pset.value = 1
-#  dut.addr.value = 0
-#  dut.level.value = 7
-#  await ClockCycles(dut.clk, 1)
-#
-#  dut._log.info("check that pwm0 is fully on")
-#  for i in range(20):
-#    await ClockCycles(dut.clk, 1)
-#    # print("dut.out_pwm: {}".format(dut.pwm_out.value))
-#    assert int(dut.pwm_out.value) == 1
-#
-#
-## Reset all pwm levels to zeros.
-#async def set_all_levels_zero(dut, clock):
-#  for i in range(8):
-#    # dut._log.info("reset pwm{}".format(i))
-#    dut.pset.value = 1
-#    dut.addr.value = i
-#    dut.level.value = 0
-#    await ClockCycles(dut.clk, 1)
-#
-#  dut.pset.value = 0
-#  await ClockCycles(dut.clk, 1)
-#
-#
-#@cocotb.test()
-#async def test_pwm1_level_4(dut):
-#  dut._log.info("start")
-#  clock = Clock(dut.clk, 10, units="us")
-#  cocotb.start_soon(clock.start())
-#
-#  await set_all_levels_zero(dut, clock)
-#
-#  dut._log.info("set pwm1=4")
-#  dut.pset.value = 1
-#  dut.addr.value = 1
-#  dut.level.value = 4
-#  await ClockCycles(dut.clk, 1)
-#  dut.pset.value = 0
-#
-#  dut._log.info("check that pwm4 is at level 4")
-#  cnt = 0
-#  for i in range(70):
-#    await ClockCycles(dut.clk, 1)
-#    print("dut.out_pwm: {}".format(dut.pwm_out.value))
-#    if (int(dut.pwm_out.value) & 0x2):
-#        cnt += 1
-#  # We need exactly 4/7, so 40 from 70 cycles.
-#  assert cnt == 40
-#
-#
-#async def test_pwm_level(dut, clock, pwm, level):
-#  await set_all_levels_zero(dut, clock)
-#
-#  dut._log.info("set pwm{}={}".format(pwm, level))
-#  dut.pset.value = 1
-#  dut.addr.value = pwm
-#  dut.level.value = level
-#  await ClockCycles(dut.clk, 1)
-#  dut.pset.value = 0
-#
-#  dut._log.info("check that pwm{} is at level {}".format(pwm, level))
-#  cnt = 0
-#  for i in range(70):
-#    await ClockCycles(dut.clk, 1)
-#    # print("dut.out_pwm: {}".format(dut.pwm_out.value))
-#    mask = 1 << pwm
-#    if (int(dut.pwm_out.value) & mask):
-#        cnt += 1
-#  # We need exactly level/7, so level*10 for 70 cycles.
-#  assert cnt == level * 10
-#
-#
-#@cocotb.test()
-#async def test_all_pwm_all_levels(dut):
-#  dut._log.info("start")
-#  clock = Clock(dut.clk, 10, units="us")
-#  cocotb.start_soon(clock.start())
-#
-#  for pwm in range(8):
-#    for level in range(8):
-#      await test_pwm_level(dut, clock, pwm=pwm, level=level)
-#
-#
+
+async def reset(dut, clock):
+  dut.reset.value = 1
+  dut.cs.value = 1
+  await ClockCycles(dut.clk, 1)
+  dut.reset.value = 0
+  dut.mosi.value = 0
+
+
+async def get_level(dut, clock, idx):
+  dut.cs.value = 0
+  # Sending a read command.
+  for i in range(8):
+    dut.mosi.value = idx >> 7
+    idx = (idx << 1) & 0xFF
+    dut.sclk.value = 1
+    await ClockCycles(dut.clk, 2)
+    dut.sclk.value = 0
+    await ClockCycles(dut.clk, 2)
+
+  # Reading the result
+  dut.mosi.value = 0
+  res = 0
+  for i in range(8):
+    dut.sclk.value = 1
+    await ClockCycles(dut.clk, 2)
+    dut.sclk.value = 0
+    await ClockCycles(dut.clk, 2)
+    res = (res << 1) + dut.miso.value
+
+  dut.cs.value = 1
+  await ClockCycles(dut.clk, 2)
+  return res
+
+
+async def set_level(dut, clock, idx, level):
+  print("set_level(idx={}, level={})".format(idx, level))
+  if level > 255:
+    raise ValueError("level must fit 8 bits, but it's {}".format(level))
+
+  idx = 0x80 | (idx & 0x7F)
+  dut.cs.value = 0
+  # Sending a write command (highest bit is set).
+  for i in range(8):
+    dut.mosi.value = idx >> 7
+    idx = (idx << 1) & 0xFF
+    dut.sclk.value = 1
+    await ClockCycles(dut.clk, 2)
+    dut.sclk.value = 0
+    await ClockCycles(dut.clk, 2)
+
+  # Processing the write value.
+  res = 0
+  tmp_val = level
+  for i in range(8):
+    dut.mosi.value = tmp_val >> 7
+    tmp_val = (tmp_val << 1) & 0xFF
+    dut.sclk.value = 1
+    await ClockCycles(dut.clk, 2)
+    dut.sclk.value = 0
+    await ClockCycles(dut.clk, 2)
+    res = (res << 1) + dut.miso.value
+
+  # The returned byte is zero, because it was waiting for the value to set.
+  assert res == 0
+
+  # And now we read what we have set back.
+  dut.mosi.value = 0
+  res = 0
+  for i in range(8):
+    dut.sclk.value = 1
+    await ClockCycles(dut.clk, 2)
+    dut.sclk.value = 0
+    await ClockCycles(dut.clk, 2)
+    res = (res << 1) + dut.miso.value
+
+  # Check that it returned the new level value.
+  #print("got_level: {}, want_level: {}".format(res, level))
+  assert res == level
+
+  dut.cs.value = 1
+  await ClockCycles(dut.clk, 2)
+
+
+
+@cocotb.test()
+async def test_read_write_regs(dut):
+  dut._log.info("start")
+  clock = Clock(dut.clk, 10, units="us")
+  cocotb.start_soon(clock.start())
+
+  await reset(dut, clock)
+  dut._log.info("check that all levels are zeros after reset")
+  for i in range(7):
+    level = await get_level(dut, clock, i)
+    assert level == 0
+
+  dut._log.info("set pwm levels to non-zero values and check them")
+  level = [0, 20, 63, 100, 127, 254, 255]
+  for i in range(7):
+    await set_level(dut, clock, i, level[i])
+    cur_level = await get_level(dut, clock, i)
+    #print("i={}, cur_level: {}, want_level: {}".format(i, cur_level, level[i]))
+    assert cur_level == level[i]
+
+  dut._log.info("check actual PWM outputs")
+  cnt = [0, 0, 0, 0, 0, 0, 0]
+  for i in range(255):
+    await ClockCycles(dut.clk, 1)
+    for j in range(7):
+      val = dut.pwm_out[j].value
+      cnt[j] += val
+      #print("i={}, j={}, val={}".format(i, j, val))
+
+  #print("cnt: {}".format(cnt))
+  for i in range(7):
+    assert cnt[i] == level[i]
+
+@cocotb.test()
+async def test_read_write_regs_all_values(dut):
+  dut._log.info("start")
+  clock = Clock(dut.clk, 10, units="us")
+  cocotb.start_soon(clock.start())
+
+  await reset(dut, clock)
+  dut._log.info("check that all levels are zeros after reset")
+  for i in range(7):
+    level = await get_level(dut, clock, i)
+    assert level == 0
+
+  dut._log.info("set pwm levels to non-zero values and check them")
+  for i in range(7):
+    for j in range(256):
+      await set_level(dut, clock, i, j)
+      cur_level = await get_level(dut, clock, i)
+      #print("i={}, cur_level: {}, want_level: {}".format(i, cur_level, level[i]))
+      assert cur_level == j
+
+  dut._log.info("check actual PWM outputs")
+  cnt = [0, 0, 0, 0, 0, 0, 0]
+  for i in range(255):
+    await ClockCycles(dut.clk, 1)
+    for j in range(7):
+      val = dut.pwm_out[j].value
+      cnt[j] += val
+      #print("i={}, j={}, val={}".format(i, j, val))
+
+  #print("cnt: {}".format(cnt))
+  for i in range(7):
+    assert cnt[i] == 255
